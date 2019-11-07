6feaa396e1d9b1e075834e78f15fc9d368f8bcf7
Swissgroover <56125454+Swissgroover@users.noreply.github.com>
2019-11-07 12:12:18 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,30 @@
+/*var modal = document.getElementById("myModal");
+
+//var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+
+    console.log("test")
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+document.getElementById("myBtn").onclick = function() { myFunction() };
+
+myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content 
+function myFunction() {
+    console.log("test")
+        //document.getElementById("myDropdown").classList.toggle("show");
+}
+*/
